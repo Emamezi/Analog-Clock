fb820bdcbd9c2a727b65d69b92f37aa4a7253ab3
Mezi <emameziebebinwe@gmail.com>
2023-03-03 00:46:29 -0500

    added comments and new code for arranging numbers

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const hourHand = document.getElementById('hour');
 const minuteHand = document.getElementById('minute');
 const secondHand = document.getElementById('second');
 
-//Rotate each number about by 30 deg
-// let allNum = document.querySelectorAll('.number');
-// for (num of allNum) {
-//     num.style.transform = `rotate(${30*num.innerText}deg)`;
-// }
-
+// Rotate each number about by 30 deg
+let allNum = document.querySelectorAll('.number');
+for (num of allNum) {
+    num.style.transform = `rotate(${30*num.innerText}deg)`;
+}
+//Function to set the clock hands
 function setClock() {
     const currentDate = new Date() //gets current date
     const secondsRatio = currentDate.getSeconds() / 60
@@ -23,10 +23,12 @@ function setClock() {
 
 }
 
+//function to set the rotation of each element wrt degrees
 function setRotation(element, rotationRatio) {
     element.style.setProperty("--rotation", rotationRatio * 360);
 
 
 }
+//set clock upon windows page load
 window.onload = setClock();
 // setClock();
